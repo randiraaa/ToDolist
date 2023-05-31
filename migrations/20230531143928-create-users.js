6c11fbfd14e9a5a94b81b621b18d47b5885503b6
Randira <204060028.mhs@stmikjabar.ac.id>
2023-05-31 22:42:34 +0700

    add migrations user & todolist

--- migrations/20230531143928-create-users.js
+++ migrations/20230531143928-create-users.js
@@ -12,6 +12,9 @@ module.exports = {
       username: {
         type: Sequelize.STRING,
       },
+      email: {
+        type: Sequelize.STRING,
+      },
       password: {
         type: Sequelize.STRING,
       },
