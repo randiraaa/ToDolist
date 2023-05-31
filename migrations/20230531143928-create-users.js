6ea4e1b23e722dac7bcab56771590e2b7e1708b8
Randira <204060028.mhs@stmikjabar.ac.id>
2023-05-31 22:16:05 +0700

    add migrations users & todolist

---
+++ migrations/20230531143928-create-users.js
@@ -0,0 +1,35 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      username: {
+        type: Sequelize.STRING,
+      },
+      password: {
+        type: Sequelize.STRING,
+      },
+      roles: {
+        type: Sequelize.ENUM("admin", "user"),
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("users");
+  },
+};
