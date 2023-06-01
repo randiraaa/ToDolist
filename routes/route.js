010f06e993cb78732e1b0f3aae5ca1dd01e19b91
Randira <204060028.mhs@stmikjabar.ac.id>
2023-06-01 14:41:42 +0700

    add route get api todosId

--- routes/route.js
+++ routes/route.js
@@ -2,6 +2,9 @@
 
 module.exports = (app) => {
   const jsonData = require("../controllers/controller-res");
+  const jsonDataTodos = require("../controllers/controller-todos");
 
   app.route("/").get(jsonData.index);
+  app.route("/todos").get(jsonDataTodos.allTodos);
+  app.route("/todos/:id").get(jsonDataTodos.todosId);
 };
