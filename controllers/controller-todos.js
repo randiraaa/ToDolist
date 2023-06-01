5a09328163dff9c44dd273b8ecec493d3771c394
Randira <204060028.mhs@stmikjabar.ac.id>
2023-06-01 14:38:42 +0700

    add controllers api get todos by id

--- controllers/controller-todos.js
+++ controllers/controller-todos.js
@@ -18,6 +18,27 @@ exports.allTodos = async (req, res) => {
     response.ok(rows, res);
   } catch (error) {
     console.log(error);
-    response.error("Error Get All Todos!", res);
+    response.error("Error Get All TODOS!", res);
+  }
+};
+
+// Menampilkan TODOS berdasarkan id
+exports.todosId = async (req, res) => {
+  try {
+    let id = req.params.id;
+    const query = "SELECT * FROM todolist WHERE id = ?";
+    const rows = await new Promise((resolve, reject) => {
+      connection.query(query, [id], (error, rows, fields) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(rows);
+        }
+      });
+    });
+    response.ok(rows, res);
+  } catch (error) {
+    console.log(error);
+    response.error("Failed to select TODOS!", res);
   }
 };
