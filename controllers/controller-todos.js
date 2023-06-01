b44e5e6cfd50375d994fbb5602fa8c68cd77d6e9
Randira <204060028.mhs@stmikjabar.ac.id>
2023-06-01 16:51:40 +0700

    add controllers api for update todos

--- controllers/controller-todos.js
+++ controllers/controller-todos.js
@@ -63,3 +63,25 @@ exports.addTodos = async (req, res) => {
     response.error("Failed to add TODOS!", res);
   }
 };
+
+// Mengubah TODOS berdasarkan id
+exports.updateTodos = async (req, res) => {
+  try {
+    let id = req.params.id;
+    let description = req.body.description;
+    const query = "UPDATE todolist SET description = ? WHERE id = ?";
+    const rows = await new Promise((resolve, reject) => {
+      connection.query(query, [description, id], (error, rows, fields) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(rows);
+        }
+      });
+    });
+    response.ok("Update TODOS success!", res);
+  } catch (error) {
+    console.log(error);
+    response.error("Failed update TODOS!", res);
+  }
+};
