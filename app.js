b56ad77073d636e06ea6803a7d86860c6826e21e
Randira <204060028.mhs@stmikjabar.ac.id>
2023-06-01 12:42:18 +0700

    routes db

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+const routes = require("./routes/route");
+routes(app);
+
 app.listen(port, () => {
   console.log(`Server started on port ${port}...`);
 });
