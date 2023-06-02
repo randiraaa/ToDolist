0b822c183c22822ecb254d5bdc661c81e6d2fe10
Randira <204060028.mhs@stmikjabar.ac.id>
2023-06-02 08:43:22 +0700

    add route api register user

--- routes/route.js
+++ routes/route.js
@@ -3,6 +3,7 @@
 module.exports = (app) => {
   const jsonData = require("../controllers/controller-res");
   const jsonDataTodos = require("../controllers/controller-todos");
+  const jsonDataUser = require("../controllers/controller-users");
 
   app.route("/").get(jsonData.index);
   app.route("/todos").get(jsonDataTodos.allTodos);
@@ -10,4 +11,5 @@ module.exports = (app) => {
   app.route("/todos/add").post(jsonDataTodos.addTodos);
   app.route("/todos/update/:id").put(jsonDataTodos.updateTodos);
   app.route("/todos/delete/:id").delete(jsonDataTodos.deleteTodos);
+  app.route("/register").get(jsonDataUser.register);
 };
