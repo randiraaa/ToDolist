d39ee6286dcbbf2b920c0c72046f9a56dd26c479
Randira <204060028.mhs@stmikjabar.ac.id>
2023-06-03 14:22:25 +0700

    add api login & auth

--- controllers/controller-users.js
+++ controllers/controller-users.js
@@ -4,6 +4,7 @@ const bcrypt = require("bcrypt");
 const connection = require("../config/db");
 const response = require("../routes/res");
 const tokenSecret = require("../config/secret");
+const ip = require("ip");
 
 // Register
 exports.register = async (req, res) => {
@@ -45,9 +46,116 @@ exports.register = async (req, res) => {
           }
         });
       });
-      response.ok("Register success!", res);
+      res.json({ success: true, message: "Register success!" });
     } else {
       response.ok("Email already exists!", res);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Login
+exports.login = async (req, res) => {
+  try {
+    const post = {
+      email: req.body.email,
+      password: req.body.password,
+    };
+
+    let query = "SELECT * FROM ?? WHERE email = ?";
+    let table = ["users", post.email];
+    query = mysql.format(query, table);
+
+    const rows = await new Promise((resolve, reject) => {
+      connection.query(query, async (error, rows) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(rows);
+        }
+      });
+    });
+
+    if (rows.length == 1) {
+      const passwordMatch = await bcrypt.compare(post.password, rows[0].password);
+
+      if (passwordMatch) {
+        const userId = rows[0].id;
+
+        let query = "SELECT * FROM ?? WHERE id = ?";
+        let table = ["tokenauth", userId];
+        query = mysql.format(query, table);
+
+        const existingTokens = await new Promise((resolve, reject) => {
+          connection.query(query, (error, rows) => {
+            if (error) {
+              reject(error);
+            } else {
+              resolve(rows);
+            }
+          });
+        });
+
+        if (existingTokens.length > 0) {
+          // Verifikasi token menghapus yang ada & mengganti baru
+          let deleteQuery = "DELETE FROM ?? WHERE id = ?";
+          let deleteTable = ["tokenauth", userId];
+          deleteQuery = mysql.format(deleteQuery, deleteTable);
+
+          await new Promise((resolve, reject) => {
+            connection.query(deleteQuery, (error, rows) => {
+              if (error) {
+                reject(error);
+              } else {
+                resolve(rows);
+              }
+            });
+          });
+        }
+
+        let token = jwt.sign({ id: rows[0].id }, tokenSecret.secret, {
+          expiresIn: 1440,
+        });
+        const id = rows[0].id;
+
+        let data = {
+          id: id,
+          access_token: token,
+          ip_address: ip.address(),
+        };
+
+        let insertTokenQuery = "INSERT INTO ?? SET ?";
+        let insertTokenTable = ["tokenauth"];
+        insertTokenQuery = mysql.format(insertTokenQuery, insertTokenTable);
+
+        await new Promise((resolve, reject) => {
+          connection.query(insertTokenQuery, data, (error, rows) => {
+            if (error) {
+              reject(error);
+            } else {
+              resolve(rows);
+            }
+          });
+        });
+
+        res.json({
+          success: true,
+          message: "Token generated successfully!",
+          token: token,
+          currUser: data.id,
+        });
+      } else {
+        res.json({
+          error: true,
+          message: "Incorrect email or password!",
+        });
+      }
+    } else {
+      res.json({
+        error: true,
+        message: "Incorrect email or password!",
+      });
     }
   } catch (error) {
     console.log(error);
