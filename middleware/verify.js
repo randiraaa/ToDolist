a47cba5a8dfb1f5ce694488404c54664137c4db9
Randira <204060028.mhs@stmikjabar.ac.id>
2023-06-03 19:51:09 +0700

    add verify admin with authorization

--- middleware/verify.js
+++ middleware/verify.js
@@ -1,43 +1,26 @@
 const jwt = require("jsonwebtoken");
 const tokenSecret = require("../config/secret");
-const connection = require("../config/db");
 
 function verifyUser(roles) {
-  return async (req, res, next) => {
+  return (req, res, next) => {
     let tokenWithBearer = req.headers.authorization;
 
     if (tokenWithBearer) {
       let token = tokenWithBearer.split(" ")[1];
-      jwt.verify(token, tokenSecret.secret, async (err, decoded) => {
+      jwt.verify(token, tokenSecret.secret, (err, decoded) => {
         if (err) {
-          return res.status(401).send({ auth: false, message: "Unregistered tokens!" });
+          return res.status(401).send({ auth: false, message: "Token tidak terdaftar!" });
         } else {
-          const userId = decoded.id;
-
-          try {
-            const query = "SELECT roles FROM users WHERE id = ?";
-            const [rows] = await connection.query(query, [userId]);
-
-            if (rows.length > 0) {
-              const userRole = rows[0].roles;
-
-              if (userRole === roles) {
-                req.auth = decoded;
-                next();
-              } else {
-                return res.status(401).send({ auth: false, message: "You don't have permission to access this page (invalid token)!" });
-              }
-            } else {
-              return res.status(401).send({ auth: false, message: "User role not found!" });
-            }
-          } catch (error) {
-            console.log(error);
-            return res.status(500).send({ auth: false, message: "Internal server error!" });
+          if (roles === "admin" && decoded.roles === "admin") {
+            req.auth = decoded;
+            next();
+          } else {
+            return res.status(401).send({ auth: false, message: "Anda tidak diberi izin! Silahkan hubungi admin untuk pusat bantuan!" });
           }
         }
       });
     } else {
-      return res.status(401).send({ auth: false, message: "No tokens!" });
+      return res.status(401).send({ auth: false, message: "Tidak ada token!" });
     }
   };
 }
