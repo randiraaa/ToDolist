08f38e9bb3c97370e401f937c2894513e6469b2b
Randira <204060028.mhs@stmikjabar.ac.id>
2023-06-03 14:20:57 +0700

    add route for users

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
-const dotenv = require("dotenv");
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(morgan("dev"));
 
 const routes = require("./routes/route");
 routes(app);
